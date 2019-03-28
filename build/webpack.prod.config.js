cc3bd93e4990b53f39e2f6b0b0586b9a4ac55cdb
chenwanan@exchain.com <imeahu@163.com>
2019-03-28 17:58:08 +0800

    no message

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,1 +1,9 @@
-webpack-dev-server
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = merge(baseConfig, {
+    plugins: [
+        new cleanWebpackPlugin()
+    ]
+})
